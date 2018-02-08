d9c1fdc5059d6f8a7fb8028749edef2b2c653230
Jens Lundell <jens.lundell@aalto.fi>
2018-02-08 15:54:46 +0200

    Fixed a bug

--- grasp_learning/src/RBFNetwork.cpp
+++ grasp_learning/src/RBFNetwork.cpp
@@ -266,7 +266,12 @@ void RBFNetwork::setNoiseVariance(const double variance, const int dist) {
 			multiVarGauss2.setCovarAsDiffernceMatrix(numKernels, variance);
 		}
 	} else {
-		multiVarGauss1.setCovarAsIndentityMatrix(numKernels, variance);
+		if (dist == 1){
+			multiVarGauss1.setCovarAsIndentityMatrix(numKernels, variance);
+		}
+		else{
+			multiVarGauss2.setCovarAsIndentityMatrix(numKernels, variance);
+		}
 	}
 }
 
